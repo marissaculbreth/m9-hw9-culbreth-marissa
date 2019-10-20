72f4dc9ee0e3c8a5b847a51502c371b06d6305f0
Marissa Culbreth <mculbreth1996@ufl.edu>
2019-10-20 13:17:42 -0400

    Toggle accordion descriptions on click

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var accordionHeaders = document.querySelectorAll('#accordion .accordion-headers');
+var accordionDescriptons = document.querySelectorAll('#accordion .accordion-description');
 
+// Handles accordion dropdown action
 function expandAccordionDescription() {
+
+
+  // Handles toggle indicator
+  for (i = 0; i < accordionHeaders.length; i++) {
+    accordionHeaders[i].childNodes[3].innerHTML = '+';
+    accordionDescriptons[i].classList.remove('accordion-open');
+  }
+
+  this.nextElementSibling.classList.toggle('accordion-open');
   this.childNodes[3].innerHTML = '-';
+
 }
 
+//Add click event listener for accordion headers
 for (i = 0; i < accordionHeaders.length; i++) {
   accordionHeaders[i].addEventListener('click', expandAccordionDescription);
 }
